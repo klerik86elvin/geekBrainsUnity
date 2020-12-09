172b6e2eaaa3ad8c611a940e9b239ef60bc1ebf0
klerik86elvin <62464631+klerik86elvin@users.noreply.github.com>
2020-12-09 20:30:26 +0400

    test

---
+++ Lesson2/Assets/ManNeko_Assets/Adventurer Blake/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {        
+        Create();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Create()
+    {
+        Instantiate(obj, transform.position, Quaternion.identity);
+    }
+}
---
+++ Lesson2/Assets/ManNeko_Assets/Adventurer Blake/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+   /* private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, Time.deltaTime * 20, 10f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            m_Animator.SetBool("IsWalking", true);
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+        else m_Animator.SetBool("IsWalking", false);
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector3.forward * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up * 3 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.up * 3 * Time.deltaTime);
+        }
+    }
+
+    
+}
